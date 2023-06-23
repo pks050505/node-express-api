f14a83f03cc97958441d07013141379a6e0f5f33
purushottam kumar <pks050505@gmail.com>
2023-06-23 16:20:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5500
 
 app.get('/', (req, res) => {
-    res.send('Hello, World! bro ');
+    res.send('dhiraj  bro  kya hal h? CICD sikhh gaye h github->cloudBuild ->DockerImage generate -> Cloud Run (Google Cloud)-> display on server');
 });
 app.get("/hello", (req, res) => {
     res.json({ "hello": "wordl" });
