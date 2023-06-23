2af77e4e4d7c4bdbb0c66789da12c0df2907b62a
purushottam kumar <pks050505@gmail.com>
2023-06-23 16:01:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const app = express();
 const PORT = process.env.PORT || 5500
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Hello, World! bro ');
 });
 app.get("/hello", (req, res) => {
     res.json({ "hello": "wordl" });
 });
+
 
 app.listen(PORT, () => {
     console.log('LINTENING ON PORT');
